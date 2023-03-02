7469dc7b29767a0cad85936607ddafa6c5b584ff
randomperson190 <53307937+randomperson190@users.noreply.github.com>
2023-03-02 15:31:20 -0300

    Update usersToFollow.user.js

--- usersToFollow.user.js
+++ usersToFollow.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram AutoFollow
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @updateURL    https://github.com/mygithubaccount/test/raw/master/test.user.js
@@ -25,7 +25,106 @@ function main() {
         let data = `biancadelfi43
         zoe.vipp1
         joseffina.morales
-        fanxpiotaa`
+        fanxpiotaa
+        alexiaperez._
+        sofibaudraco
+        ludrdriguez
+        pauilio_
+        pauilio_
+        nahii_herrera
+        sool.portilloo
+        luuzfusto
+        danigutiierrez_
+        alegabriiel_
+        seleceballos_
+        brenavaca
+        nattypereyra_
+        mmaurios
+        morecampoy
+        daiianaa._5
+        maairepic
+        maca_cuevass
+        valeenpalma
+        cattapereyra
+        abrilautunno
+        shaiel__
+        nikiivega
+        camiialbarracin_
+        veroayoroaj
+        nattypereyra_
+        brenavaca
+        morecampoy
+        daiianaa._5
+        daiianaa._5
+        maairepic
+        zoesarubi_
+        luaguiirre_
+        she.is.lolaa
+        luaguiirre_
+        lourdessolis38
+        correaacele
+        magacentteno
+        maca_cuevass
+        cattapereyra
+        abrilautunno
+        shaiel__
+        melina.boch
+        briortegga
+        danaagostinaa
+        psyccony
+        agus_surita
+        sereepesaresi_
+        agus_berenguel
+        ludmi_fuentes
+        _sofi.correa
+        cande.yanivelli
+        tomaslorenzoma11037
+        catluquez
+        _araa08
+        braunl_
+        moreparera_
+        micaorosco_
+        micaorosco_
+        milydomiinguez
+        mmorena.1
+        niji_romkvzfl
+        giulygonz
+        _luu.datri_
+        naycascone
+        camii__st
+        kiruscapina
+        zoepnzani_
+        _ceci4
+        maiochoaizpuro
+        ilianagrillo
+        eugeniaszwec_
+        ayee.tejera
+        chabeaccossato
+        azuu.borgna
+        mayvergara__
+        abrilrigotto01
+        checchuu_
+        _jjazzminn_
+        vaal.0n
+        aix.iaraa
+        daidieci_
+        _camiiperaltaa
+        agustinaprette_
+        iaraa_brahim
+        melveronn1
+        juuliherrera_
+        celesrodriguez._
+        magui.lucila
+        alderetebri_
+        delfiilescano_
+        _fabiii.10_
+        mari.llopiz
+        meloviiedo
+        melany.rocio2
+        guadaa_nicole
+        biangiovannini
+        vicky__gimenez0
+        _mariaperalta_`
         let usersToFollow = data.split("\n");
         let date = new Date;
         let hours = date.getHours();
