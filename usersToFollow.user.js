3fb1627408cbead6c436382a62f258cc14938c67
randomperson190 <53307937+randomperson190@users.noreply.github.com>
2023-03-09 15:56:56 -0300

    Update usersToFollow.user.js

--- usersToFollow.user.js
+++ usersToFollow.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram AutoFollow
 // @namespace    http://tampermonkey.net/
-// @version      0.36
+// @version      0.37
 // @description  try to take over the world!
 // @author       You
 // @updateURL    https://github.com/randomperson190/usersToFollow/raw/main/usersToFollow.user.js
